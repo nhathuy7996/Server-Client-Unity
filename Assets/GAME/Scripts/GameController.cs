91137e11a8f42c31f9eba33b7c023a27309ff487
nhathuy7996 <nhat.huy.7996@gmail.com>
2025-11-11 17:38:37 +0700

    clean callback

--- Assets/GAME/Scripts/GameController.cs
+++ Assets/GAME/Scripts/GameController.cs
@@ -40,140 +40,132 @@ public class GameController : MonoBehaviour
     {
         Debug.Log("Connected to server at " + Module.baseUrl);
         NetworkingPeer.Instant.EmmitEvent("startGame", "{}");
-        NetworkingPeer.Instant.ListenEvent("game:joined", (data) =>
-        {
+        NetworkingPeer.Instant.ListenEvent("game:joined", OnPlayerJoined);
 
-            var dataJson = SimpleJSON.JSON.Parse(data);
-            playerID = dataJson["playerId"].AsInt;
-            UnityMainThread.wkr.AddJob(() =>
-            {
-                var player = Instantiate(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-                camControl.target = player.transform;
-            });
+        NetworkingPeer.Instant.ListenEvent("game:playerJoined", OnOtherPlayerJoined);
 
-        });
-
-        NetworkingPeer.Instant.ListenEvent("game:playerJoined", (data) =>
-       {
-
-           var dataJson = SimpleJSON.JSON.Parse(data);
-           var playerID = dataJson["playerId"].AsInt;
-           if (this.playerID == playerID)
-           {
-               return;
-           }
-           UnityMainThread.wkr.AddJob(() =>
-           {
-               if (!this.players.ContainsKey(playerID))
-               {
-                   var player = Instantiate(otherPlayerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-                   player.name = "OtherPlayer_" + playerID;
-                   player.playerID = playerID;
-                   this.players.Add(playerID, player);
-               }
-           });
-
-       });
-
-        NetworkingPeer.Instant.ListenEvent("game:playerLeft", (data) =>
-     {
-
-         var dataJson = SimpleJSON.JSON.Parse(data);
-         var playerID = dataJson["playerId"].AsInt;
-         if (!this.players.ContainsKey(playerID))
-         {
-             return;
-         }
-         UnityMainThread.wkr.AddJob(() =>
-         {
-             Destroy(this.players[playerID].gameObject);
-             this.players.Remove(playerID);
-         });
-
-     });
+        NetworkingPeer.Instant.ListenEvent("game:playerLeft", OnPlayerLeft);
 
 
-        NetworkingPeer.Instant.ListenEvent("game:allPlayersState", (data) =>
-        {
+        NetworkingPeer.Instant.ListenEvent("game:allPlayersState", OnGetAllPlayersState);
 
-            var dataJson = SimpleJSON.JSON.Parse(data);
-            var playersArray = dataJson["players"].AsArray;
-            UnityMainThread.wkr.AddJob(() =>
-            {
-                for (int i = 0; i < playersArray.Count; i++)
-                {
-                    var playerData = playersArray[i];
-                    var playerID = playerData["id"].AsInt;
-                    if (this.playerID == playerID)
-                    {
-                        continue;
-                    }
-
-                    if (!this.players.ContainsKey(playerID))
-                    {
-                        var player = Instantiate(otherPlayerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-                        player.name = "OtherPlayer_" + playerID;
-                        player.playerID = playerID;
-                        this.players.Add(playerID, player);
-                    }
-
-                    var position = new Vector3(playerData["position"]["x"].AsFloat, playerData["position"]["y"].AsFloat, playerData["position"]["z"].AsFloat);
-                    this.players[playerID].transform.position = position;
-
-                }
-
-            });
-        });
-
-        NetworkingPeer.Instant.ListenEvent("game:playerUpdates", (data) =>
-       {
-
-           var dataJson = SimpleJSON.JSON.Parse(data);
-           UnityMainThread.wkr.AddJob(() =>
-           {
-               for (int i = 0; i < dataJson.AsArray.Count; i++)
-               {
-                   var playerData = dataJson.AsArray[i];
-                   var playerID = playerData["id"].AsInt;
-                   if (this.playerID == playerID)
-                   {
-                       continue;
-                   }
-
-                   if (!this.players.ContainsKey(playerID))
-                   {
-                       var player = Instantiate(otherPlayerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-                       player.name = "OtherPlayer_" + playerID;
-                       player.playerID = playerID;
-                       this.players.Add(playerID, player);
-                   }
-
-                   if (playerData["position"] != null)
-                   {
-                       var position = new Vector3(playerData["position"]["x"].AsFloat, playerData["position"]["y"].AsFloat, playerData["position"]["z"].AsFloat);
-                       this.players[playerID].transform.position = position;
-                   }
-
-                   if (playerData["velocity"] != null)
-                   {
-
-                       var velocity = new Vector3(playerData["velocity"]["x"].AsFloat, playerData["velocity"]["y"].AsFloat, playerData["velocity"]["z"].AsFloat);
-                       this.players[playerID].velocity = velocity;
-
-                       // Rotate player to face velocity direction
-                       if (velocity.magnitude > 0.1f)
-                       {
-                           this.players[playerID].transform.rotation = Quaternion.LookRotation(velocity.normalized);
-                       }
-                   }
-
-               }
-           });
-
-       });
+        NetworkingPeer.Instant.ListenEvent("game:playerUpdates", OnPlayerUpdate);
 
 
     }
 
 
+    void OnPlayerJoined(string data)
+    {
+        var dataJson = SimpleJSON.JSON.Parse(data);
+        playerID = dataJson["playerId"].AsInt;
+        UnityMainThread.wkr.AddJob(() =>
+        {
+            var player = Instantiate(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            camControl.target = player.transform;
+        });
+    }
+
+    void OnOtherPlayerJoined(string data)
+    {
+        var dataJson = SimpleJSON.JSON.Parse(data);
+        var playerID = dataJson["playerId"].AsInt;
+        if (this.playerID == playerID)
+        {
+            return;
+        }
+        UnityMainThread.wkr.AddJob(() => AutoFillPlayerNotExist(otherPlayerPrefab, playerID));
+    }
+
+    void OnPlayerLeft(string data)
+    {
+        var dataJson = SimpleJSON.JSON.Parse(data);
+        var playerID = dataJson["playerId"].AsInt;
+        if (!this.players.ContainsKey(playerID))
+        {
+            return;
+        }
+        UnityMainThread.wkr.AddJob(() =>
+        {
+            Destroy(this.players[playerID].gameObject);
+            this.players.Remove(playerID);
+        });
+    }
+
+    void OnGetAllPlayersState(string data)
+    {
+        var dataJson = SimpleJSON.JSON.Parse(data);
+        var playersArray = dataJson["players"].AsArray;
+        UnityMainThread.wkr.AddJob(() =>
+        {
+            for (int i = 0; i < playersArray.Count; i++)
+            {
+                var playerData = playersArray[i];
+                var playerID = playerData["id"].AsInt;
+                if (this.playerID == playerID)
+                {
+                    continue;
+                }
+
+                AutoFillPlayerNotExist(otherPlayerPrefab, playerID);
+
+
+                var position = new Vector3(playerData["position"]["x"].AsFloat, playerData["position"]["y"].AsFloat, playerData["position"]["z"].AsFloat);
+                this.players[playerID].transform.position = position;
+
+            }
+
+        });
+    }
+
+    void OnPlayerUpdate(string data)
+    {
+        var dataJson = SimpleJSON.JSON.Parse(data);
+        UnityMainThread.wkr.AddJob(() =>
+        {
+            for (int i = 0; i < dataJson.AsArray.Count; i++)
+            {
+                var playerData = dataJson.AsArray[i];
+                var playerID = playerData["id"].AsInt;
+                if (this.playerID == playerID)
+                {
+                    continue;
+                }
+
+                AutoFillPlayerNotExist(otherPlayerPrefab, playerID);
+
+                if (playerData["position"] != null)
+                {
+                    var position = new Vector3(playerData["position"]["x"].AsFloat, playerData["position"]["y"].AsFloat, playerData["position"]["z"].AsFloat);
+                    this.players[playerID].transform.position = position;
+                }
+
+                if (playerData["velocity"] != null)
+                {
+
+                    var velocity = new Vector3(playerData["velocity"]["x"].AsFloat, playerData["velocity"]["y"].AsFloat, playerData["velocity"]["z"].AsFloat);
+                    this.players[playerID].velocity = velocity;
+
+                    // Rotate player to face velocity direction
+                    if (velocity.magnitude > 0.1f)
+                    {
+                        this.players[playerID].transform.rotation = Quaternion.LookRotation(velocity.normalized);
+                    }
+                }
+
+            }
+        });
+
+    }
+
+    void AutoFillPlayerNotExist(PlayerData playerPrefab, int playerID)
+    {
+        if (!this.players.ContainsKey(playerID))
+        {
+            var player = Instantiate(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            player.name = "Player_" + playerID;
+            player.playerID = playerID;
+            this.players.Add(playerID, player);
+        }
+    }
 }
